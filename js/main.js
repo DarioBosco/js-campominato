62101e4d4adba2cf050c2a8aac43217e3a1aaec0
Dario Bosco <dario_bosco@protonmail.com>
2021-07-26 17:57:11 +0200

    Aggiunti cicli do while a gestione delle iterazioni per controllo dei valori

--- js/main.js
+++ js/main.js
@@ -2,18 +2,21 @@
 TODO:
 //Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
 //I numeri non possono essere duplicati.
-In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-L’utente non può inserire più volte lo stesso numero.
+//In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+//L’utente non può inserire più volte lo stesso numero.
 Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 */
 
 const computerNumbers = [];
+const userNumbers = [];
 /* Range per la generazione casuale dei numeri */
 var iterations = 16;
 var min = 1;
 var max = 100;
+
+var userChoices = max - iterations;
 
 /* 
 Il computer deve generare 16 numeri casuali (le nostre bombe) tra 1 e 100.
@@ -21,13 +24,25 @@ I numeri non possono essere duplicati.
 */
 
 for (let i = 0; i < iterations; i++) {
-	var random = getRandomInt(min, max);
-	if (computerNumbers.includes(random)) {
-		random = getRandomInt(min, max);
-	}
+	do {
+		var random = getRandomInt(min, max);
+	} while (computerNumbers.includes(random));
 	computerNumbers.push(random);
 }
 console.log(computerNumbers);
+
+/* 
+In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+L’utente non può inserire più volte lo stesso numero.
+*/
+
+for (let i = 0; i < userChoices; i++) {
+	do {
+		var inputNumber = parseInt(prompt('Inserisci un numero da 1 a 100'));
+	} while (userNumbers.includes(inputNumber) || inputNumber > max || inputNumber < min);
+	userNumbers.push(inputNumber);
+	console.log(userNumbers);
+}
 
 /* Funzioni */
 function getRandomInt(min, max) {
